c04f265208611daeb0a6ea98a9c8d622ca6f6265
FMakareev <f.makareev@compaero.ru>
2018-09-17 14:06:51 +0300

    add gtag.js

--- src/server/html.js
+++ src/server/html.js
@@ -1,69 +1,85 @@
-import React,{Fragment} from 'react';
+import React, {Fragment} from 'react';
 import PropTypes from 'prop-types';
 import serialize from 'serialize-javascript'
 
 
-const Html = ({content,request,title,cache,style,reduxState,asyncState,getStatic = false}) => (
-    <html lang="en">
-    <head>
-        <meta charSet="utf-8"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+const Html = ({content, request, title, cache, style, reduxState, asyncState, getStatic = false}) => (
+  <html lang="en">
+  <head>
+    <meta charSet="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
 
-        <title>Emernotar.io: {title}</title>
-            <style></style>
-    </head>
-    <body>
-    <div id="app" dangerouslySetInnerHTML={{__html: content}}/>
-    {/*{*/}
-        {/*cache && <script*/}
-            {/*charSet="UTF-8"*/}
-            {/*dangerouslySetInnerHTML={{*/}
-                {/*__html: `window.__APOLLO_STATE__=${JSON.stringify(cache.extract())};`,*/}
-            {/*}}*/}
-        {/*/>*/}
-    {/*}*/}
-    {
-        reduxState &&
-        <script
-            charSet="UTF-8"
-            dangerouslySetInnerHTML={{
-                __html: `window.__PRELOADED_REDUX_STATE__=${JSON.stringify(reduxState)};`,
-            }}
-        />
-    }
-    {
-        asyncState &&
-        <script
-            charSet="UTF-8"
-            dangerouslySetInnerHTML={{
-                __html: `window.__ASYNC_COMPONENTS_STATE__=${serialize(asyncState)};`,
-            }}
-        />
-    }
+    <title>Emernotar.io: {title}</title>
+    <style></style>
+    {/*<!-- Global site tag (gtag.js) - Google Analytics -->*/}
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-125870443-1"/>
+    <script
+      type="text/javascript"
+      charSet="UTF-8"
+      dangerouslySetInnerHTML={{
+        __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
 
-    {
-        !getStatic && <script src="/bundle.js" charSet="UTF-8"/>
-    }
+        gtag('config', 'UA-125870443-1');
+          `
+      }}
+    />
 
-    </body>
-    </html>
+  </head>
+  <body>
+  <div id="app" dangerouslySetInnerHTML={{__html: content}}/>
+  {/*{*/}
+  {/*cache && <script*/}
+  {/*charSet="UTF-8"*/}
+  {/*dangerouslySetInnerHTML={{*/}
+  {/*__html: `window.__APOLLO_STATE__=${JSON.stringify(cache.extract())};`,*/}
+  {/*}}*/}
+  {/*/>*/}
+  {/*}*/}
+  {
+    reduxState &&
+    <script
+      charSet="UTF-8"
+      dangerouslySetInnerHTML={{
+        __html: `window.__PRELOADED_REDUX_STATE__=${JSON.stringify(reduxState)};`,
+      }}
+    />
+  }
+  {
+    asyncState &&
+    <script
+      charSet="UTF-8"
+      dangerouslySetInnerHTML={{
+        __html: `window.__ASYNC_COMPONENTS_STATE__=${serialize(asyncState)};`,
+      }}
+    />
+  }
+
+  {
+    !getStatic && <script src="/bundle.js" charSet="UTF-8"/>
+  }
+
+  </body>
+  </html>
 );
 Html.propTypes = {
-    content: PropTypes.string,
-    client: PropTypes.shape({
-        cache: PropTypes.object,
-    }),
-    reduxState: PropTypes.string,
-    style: PropTypes.string,
-    asyncState: PropTypes.string,
+  content: PropTypes.string,
+  client: PropTypes.shape({
+    cache: PropTypes.object,
+  }),
+  reduxState: PropTypes.string,
+  style: PropTypes.string,
+  asyncState: PropTypes.string,
 };
 Html.defaultProps = {
-    content: null,
-    client: null,
-    reduxState: null,
-    style: null,
-    asyncState: null,
+  content: null,
+  client: null,
+  reduxState: null,
+  style: null,
+  asyncState: null,
 };
 
 export default Html;
